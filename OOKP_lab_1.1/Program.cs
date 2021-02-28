00aa0b1af8314d48802466e630b7399d0b73eace
Yaroslav <history.shots19@gmail.com>
2021-02-28 15:42:19 +0200

    Add project files.

---
+++ OOKP_lab_1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOKP_lab_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1 test1 = new Test1();
+            Test2 test2 = new Test2();
+            test1.Print();
+            test2.Print();
+            Console.ReadKey();
+        }
+
+    }
+    class Test1
+    {
+        public void Print()
+        {
+            Console.WriteLine("Hello from class 1");
+        }
+    }
+    class Test2
+    {
+        public void Print()
+        {
+            Console.WriteLine("Hello from class 2");
+        }
+    }
+}
